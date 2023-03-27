fd49790cf696e1ef0273e0154f6efdddecd70c35
NaomiL1 <Nlauture2@gmail.com>
2023-03-27 18:09:20 -0400

    Changes
    Menu toggle and addition of html page (teacart)

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function menuToggle () {
     var x = document.getElementById ('myNavtoggle');
     if (x.className === 'navtoggle') {
-        x.className += ' resonsive';
+        x.className += ' responsive';
     }   else {
         x.className = 'navtoggle';
     }
